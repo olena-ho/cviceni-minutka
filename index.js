a7c06f7e0875024af1b6a976e53842d6d832afe6
Olena Horovenko <elena.gorovenko@gmail.com>
2024-05-03 20:19:09 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('funguju!');
+//setTimeout(addRing, 5000)
+//select element 
+//create a function to add a class to the element
+
+const alarmEl = document.querySelector('.alarm');
+
+const time = prompt('set the alarm for how many seconds: ')
+
+const audioEl = document.querySelector('audio');
+
+const addRing = () => {
+  alarmEl.classList.add('alarm--ring');
+  audioEl.play()
+}
+
+const ringTimer = setTimeout(addRing, time * 1000)
+
+//enabling cancel
+const cancelEl = document.querySelector('.cancel');
+
+const cancelTimer = () => {
+  clearTimeout(ringTimer);
+};
+
+cancelEl.addEventListener('click', cancelTimer)
